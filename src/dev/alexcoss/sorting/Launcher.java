67b15427fd173e55b533485277a3898dd5795ee9
alexcoss <kozachenko.aleksander@gmail.com>
2023-05-08 12:34:13 +0200

    init commit

---
+++ src/dev/alexcoss/sorting/Launcher.java
@@ -0,0 +1,11 @@
+package dev.alexcoss.sorting;
+
+/**
+ * This is a small project with various sorting algorithms such as: Bubble Sort, Selection Sort, Insertion Sort, Quick Sort, Merge Sort.
+ * @author alexcoss
+ * */
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
